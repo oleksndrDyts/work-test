ab4dbf6b49b3911df200f35b41903cd3bd1b4f47
SashaDyts <lentrofan@gmail.com>
2023-02-21 19:44:17 +0200

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3003/api',
+  baseURL: 'https://work-testback-production.up.railway.app/api',
 });
 
 const setToken = token => {
